43e55ee69f40f6ab59b9fb5b6de9e8c59f9f0c3a
Aychingva <aychinguliyeva@gmail.com>
2026-05-02 13:46:04 +0000

    Add docstrings to 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""Module that calculates the shape of a matrix"""
+
+
 def matrix_shape(matrix):
+    """Calculates the shape of a matrix and returns it as a list of integers"""
     shape = []
     current = matrix
     while isinstance(current, list):
