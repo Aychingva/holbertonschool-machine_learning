cdcfcaa082243b06cf8996c5d5397a93c53b2bee
Aychingva <aychinguliyeva@gmail.com>
2026-05-28 22:23:06 +0400

    Fix pycodestyle E302 - 2 blank lines before function

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -2,16 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def scatter():
 
+def scatter():
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
     plt.figure(figsize=(6.4, 4.8))
-
-    # your code here
     plt.scatter(x, y, color='magenta')
     plt.xlabel('Height (in)')
     plt.ylabel('Weight (lbs)')
