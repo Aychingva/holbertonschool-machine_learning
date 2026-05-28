ad61a92e58177aad7d19513a6cf6bceb861baf22
Aychingva <aychinguliyeva@gmail.com>
2026-05-28 21:21:04 +0400

    Add 2-from_file

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for loading data from a file as a pd.DataFrame."""
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """Load data from a file as a pd.DataFrame.
+
+    Args:
+        filename: the file to load from
+        delimiter: the column separator
+
+    Returns:
+        the loaded pd.DataFrame
+    """
+    return pd.read_csv(filename, sep=delimiter)
