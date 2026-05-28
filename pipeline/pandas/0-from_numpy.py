7645c41e5c692761baa7fc9211ac9bbca39fba51
Aychingva <aychinguliyeva@gmail.com>
2026-05-28 21:17:51 +0400

    Add module and function docstrings

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+"""Module for creating a pandas DataFrame from a numpy ndarray."""
 import pandas as pd
 
 
 def from_numpy(array):
+    """Create a pd.DataFrame from a np.ndarray.
+
+    Args:
+        array: the np.ndarray to convert
+
+    Returns:
+        the newly created pd.DataFrame
+    """
     cols = [chr(65 + i) for i in range(array.shape[1])]
     return pd.DataFrame(array, columns=cols)
